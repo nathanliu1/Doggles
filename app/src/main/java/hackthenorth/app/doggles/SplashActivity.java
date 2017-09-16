de35a363be2843ba0ea5385d735bfeb59c6be211
Nathan Liu <zero_2_6@hotmail.com>
2017-09-15 22:54:53 -0700

    inital setup

---
+++ app/src/main/java/hackthenorth/app/doggles/SplashActivity.java
@@ -0,0 +1,17 @@
+package hackthenorth.app.doggles;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zero_ on 9/15/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
