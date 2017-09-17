527dd5a8cd9b55720f15b6a9fce7a42d194c4326
Fanxing Zhang <fanxing_zhang1@live.ca>
2017-09-17 01:00:13 -0400

    json parser

--- app/src/main/java/hackthenorth/app/doggles/MainActivity.java
+++ app/src/main/java/hackthenorth/app/doggles/MainActivity.java
@@ -18,6 +18,10 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -56,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoURI);
             startActivityForResult(takePictureIntent, TAKE_PICTURE);
         }
-        new ImageRecognitionService().execute();
     }
 
     // TODO: Move this elsewhere + add real image file.
@@ -67,10 +70,11 @@ public class MainActivity extends AppCompatActivity {
             VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
             service.setApiKey("f3225891c60c0abe5a2424102fdf772d1d9955d4");
             ClassifyImagesOptions options = new ClassifyImagesOptions.Builder()
-                    .images(new File("IMAGE FILE"))
+                    .images(new File(mCurrentPhotoPath))
                     .build();
             VisualClassification result = service.classify(options).execute();
-            System.out.println(result);
+            parseResponse(result.toString());
+            //System.out.println(result.toString());
             return null;
         }
     }
@@ -100,7 +104,33 @@ public class MainActivity extends AppCompatActivity {
 
                 // Start the animation (looped playback by default).
                 frameAnimation.start();
+
+                new ImageRecognitionService().execute();
             }
         }
+    }
+
+    public void parseResponse(String response) {
+        try {
+            JSONObject resultJson = new JSONObject(response);
+            JSONArray images = resultJson.getJSONArray("images");
+            for (int k = 0; k < images.length(); k++) {
+                JSONObject image = images.getJSONObject(k);
+                JSONArray classifiers = image.getJSONArray("classifiers");
+                for (int i = 0; i < classifiers.length(); i++) {
+                    JSONObject classifiersJSONObject = classifiers.getJSONObject(i);
+                    JSONArray classes = classifiersJSONObject.getJSONArray("classes");
+                    for (int j = 0; j < classes.length(); j++) {
+                        JSONObject classObject = classes.getJSONObject(j);
+                        if (classObject.getString("class").contains("dog")) {
+                            Log.d("RESULT: ", classObject.getString("class"));
+                        }
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
